dde5da02c1c47d6d568befde1ed2b9b6376a824e
Kompot <69313786+kompot69@users.noreply.github.com>
2023-02-26 19:02:35 +0300

    Add files via upload

---
+++ FixLayout_2.py
@@ -0,0 +1,25 @@
+# Код спизжен из модуля print, автора KeyZenD
+# Govnocode by @kompot_69
+from .. import loader, utils
+@loader.tds
+class FixLayout(loader.Module):
+	"""Фиксит раскладку"""
+	strings = {"name": "Fix Layout"}
+	@loader.owner
+	async def printcmd(self, message):
+		""".fl <text or reply>"""
+		layout = dict(zip(map(ord, 
+			"qwertyuiop[]asdfghjkl;'zxcvbnm,./`"
+            'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'),
+            "йцукенгшщзхъфывапролджэячсмитьбю.ё"
+            'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё'))
+		text = utils.get_args_raw(message)
+		if not text:
+			reply = await message.get_reply_message()
+			if not reply or not reply.message:
+				await message.edit("<b>Текста нет!</b>")
+				return
+			text = reply.message
+		text = text.translate(layout)
+		await message.edit(text)
+			
