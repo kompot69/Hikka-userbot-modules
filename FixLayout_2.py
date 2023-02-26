eb9431f0d3f319ff4c14cc08581cc5d516de353d
Kompot <69313786+kompot69@users.noreply.github.com>
2023-02-26 19:33:32 +0300

    Update FixLayout_2.py

--- FixLayout_2.py
+++ FixLayout_2.py
@@ -7,8 +7,20 @@ class FixLayoutMod(loader.Module):
 	strings = {"name": "Fix Layout"}
 	@loader.owner
 	async def flcmd(self, message):
-		""".fl <text or reply>"""
+		""".fl <text or reply> en>ru"""
 		layout = dict(zip(map(ord, "qwertyuiop[]asdfghjkl;'zxcvbnm,./`" 'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'), "йцукенгшщзхъфывапролджэячсмитьбю.ё" 'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё'))
+		text = utils.get_args_raw(message)
+		if not text:
+			reply = await message.get_reply_message()
+			if not reply or not reply.message:
+				await message.edit("<b>Текста нет!</b>")
+				return
+			text = reply.message
+		text = text.translate(layout)
+		await message.edit(text)
+	async def flrcmd(self, message):
+		""".flr <text or reply> ru>en"""
+		layout = dict(zip(map(ord, "йцукенгшщзхъфывапролджэячсмитьбю.ё" 'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё'), "qwertyuiop[]asdfghjkl;'zxcvbnm,./`" 'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'))
 		text = utils.get_args_raw(message)
 		if not text:
 			reply = await message.get_reply_message()
