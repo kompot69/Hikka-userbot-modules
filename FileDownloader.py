f0f2a21451d4af274630dc602440350cb37191f8
Kompot <69313786+kompot69@users.noreply.github.com>
2024-01-14 21:11:05 +0300

    Add files via upload

--- FileDownloader.py
+++ FileDownloader.py
@@ -1,4 +1,4 @@
-# Friendly Telegram File Downloader (Uploader) module by @kompot_69 & @mirivan
+# Friendly Telegram File Downloader (Uploader to Telegram) module by @kompot_69 & @mirivan
 # requires: requests
 import datetime as DT
 import logging
@@ -10,15 +10,14 @@ import sys
 from datetime import datetime
 from io import BytesIO
 from telethon.errors import MessageIdInvalidError, MessageNotModifiedError
+from traceback import format_exc
 from urllib.parse import urlparse
 
 
 from .. import loader, utils
 
-logger = logging.getLogger(__name__)
-
 def sizeof_fmt(num):
-    for unit in ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB"]:
+    for unit in ["B", "KB", "MB", "GB"]:
         if abs(num) < 1024.0:
             return f"{num:3.1f}{unit}"
         num /= 1024.0
@@ -27,60 +26,60 @@ def sizeof_fmt(num):
 
 @loader.tds
 class FileDownloaderMod(loader.Module):
-    """File Downloader by @kompot_69 and @mirivan"""
     strings = {"name": "File Downloader"}
+
+    async def client_ready(self, client, db):
+        self.client = client
+        self.db = db
 
     @loader.unrestricted
     async def dlfilecmd(self, message):
-        """.dlfile <link/reply message>
-           Download file from link or reply message with url."""
         await message.edit("<b>Инициализация...</b>")
+        prefix = self.db.get("dlfile", "command_prefix", ["."])[0]
+        commands = [
+            f"<code>{prefix}dlfile [пересланное сообщение]|<URL> [текст по окончанию загрузки]</code> - скачать файл по ссылке (продерживаются прямые ссылки, ссылки с перенаправлением), можно указать текст по окончанию загрузки - появляется вместо текста с информацией, по какой ссылке был скачан файл.",
+            f"<code>{prefix}dlfile <пересланное сообщение> [индекс> [текст по окончанию загрузки]</code> - скачать файл по ссылке из пересланного сообщения, можно указать индекс ссылки по которой необходимо скачать файл (должен начинаться с 1, используется только когда в сообщении присутствует более одной ссылки)."
+        ]
+        usage = "<b>Использование модуля</b>:\n" + "\n".join(commands)
         reply = await message.get_reply_message()
         arg = utils.get_args_raw(message)
-        url_regex = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+        url_regex = "((?:https?:\/\/)[A-z0-9]{2,}\.(?:[A-z]{2,})?[^\s]+[^. ])"
 
-        if reply and reply.raw_text:
-            match_reply = re.findall(url_regex, reply.raw_text)
-            match_arg = re.findall(url_regex, arg)
-            if match_arg:
-                url = match_arg[0]
-            elif len(match_reply) > 1:
-                try:
-                    try:
-                        target_match = int(arg)
-                        if target_match < 0:
-                            raise IndexError("target_match value lower than zero")
-                        url = match_reply[target_match - 1]
-                    except IndexError:
-                        return await message.edit(f"<b>Ссылка с индексом [{target_match}] в пересланном сообщении не обнаружена. Подсказка: индексация начинается от числа 1.</b>")
-                except (TypeError, ValueError):
-                    return await message.edit(f"<b>В пересланном сообщении обнаружено несколько ссылок ({len(match_reply)}) на файлы. Укажите в аргументе индекс ссылки по которой нужно загружать файл. Пример</b>: <code>.dlfile 1</code>")
-            else:
-                try:
-                    url = match_reply[0]
-                except IndexError:
-                    return await message.edit("<b>В пересланном сообщении не обнаружены ссылки.</b>")
+        match_arg = re.findall(url_regex, arg)
+        match_reply = re.findall(url_regex, reply.raw_text) if reply and reply.raw_text else None
+        if match_arg or match_reply:
+            url = match_arg[0] if match_arg else match_reply[0]
+            args = arg.split(" ", 1)
+            last_args_index = len(args) - 1
+            text = args[-1]
+            if url in args and len(args) > 1:
+                if not args.index(url) == last_args_index:
+                    text = args[-2]
+
+            if match_reply and len(match_reply) > 1:
+                index = args[0]
+                if index.isnumeric() and int(index) > 0:
+                    url = match_arg[int(index) - 1] if match_arg else match_reply[int(index) - 1]
+                else:
+                    return await message.edit(usage)
         else:
-            match_arg = re.findall(url_regex, arg)
-            if match_arg:
-                url = match_arg[0]
-            else:
-                return await message.edit("<b>Укажите ссылку в качестве аргумента или перешлите сообщение с ссылкой.</b>")
+            return await message.edit(usage)
 
         try:
-            with requests.get(url.strip(), stream=True) as r:
+            with requests.get(url.strip(), headers={'Accept-Encoding': None}, stream=True, timeout=(10, 5), verify=False) as r:
+                if r.status_code == 404:
+                    return await message.edit("<b>Ошибка 404 при запросе на URL, указанный файл не найден.</b>")
                 r.raise_for_status()
-                try:
-                    content_disposition = r.headers["Content-Disposition"]
-                    match = re.compile("filename=(.*)(| )")
-                    filename = match.search(content_disposition).group(1)
-                except (AttributeError, KeyError):
-                    filename = os.path.basename(urlparse(url).path)
-                    if not filename:
-                        return await message.edit(f"<b>Не удалось получить имя файла.</b>")
-                length = int(r.headers["Content-Length"])
-                if length > 2147483648:
-                    return await message.edit(f"<b>Файл не будет загружен</b>: его размер <code>{sizeof_fmt(length)}/2GB</code>")
+                filename = r.headers.get("Content-Disposition", "").split("filename=")[-1].split(';')[0].strip() or os.path.basename(urlparse(url).path)
+                if not filename:
+                    return await message.edit("<b>Указаный URL не ведёт на файл.</b>")
+                length = int(r.headers.get("Content-Length", 0))
+                if not length:
+                    return await message.edit("<b>Не удалось получить размер файла.</b>")
+                me = await message.client.get_me()
+                allowed_file_size = 4 if me.premium else 2
+                if length > allowed_file_size * 1024 ** 3:
+                    return await message.edit(f"<b>Файл не будет загружен, поскольку его размер (</b><code>{sizeof_fmt(length)}</code><b>) превышает допустимые для вас <code>{allowed_file_size}GB</code> для загрузки файлов.")
                 length_h = sizeof_fmt(length)
                 timestamp_old = timestamp_new = datetime.now()
                 downloaded = 0
@@ -99,23 +98,16 @@ class FileDownloaderMod(loader.Module):
                             timestamp_new += DT.timedelta(seconds=5)
                         else:
                             timestamp_old = datetime.now()
-                    else:
-                        f.close()
-                        await message.edit(f"<b>Выгружаю файл</b>: <code>{filename}</code>")
-                        try:
-                            await message.client.send_file(message.to_id, '/tmp/' + filename, caption=f"<b>Файл загружен по ссылке</b>: <code>{url}</code>", reply_to=reply)
-                            await message.delete()
-                        except Exception as e:
-                            await message.edit(f"<b>Не удалось выгрузить файл</b>: <code>{e}</code>")
-                        try:
-                            os.remove('/tmp/' + filename)
-                        except Exception:
-                            pass
+                    await message.edit(f"<b>Выгружаю файл</b>: <code>{filename}</code>")
+                    caption = text or f"<b>Файл загружен по ссылке</b>: <code>{url}</code>"
+                    await message.client.send_file(message.to_id, '/tmp/' + filename, caption=caption, reply_to=reply)
+                    await message.delete()
         except MessageIdInvalidError:
             await message.client.send_message(message.to_id, "<b>Загрузка файла остановлена пользователем.</b>", reply_to=reply)
-            try:
-                os.remove('/tmp/' + filename)
-            except Exception:
-                pass
+        except requests.exceptions.Timeout:
+            await message.edit(f"<b>Истекло время ожидания от сервера</b>: <code>{url}</code>")
         except Exception as e:
-            await message.edit(f"<b>Произошла ошибка</b>: <code>{e}</code>")
+            traceback = re.sub("(`|\\|\\||\\*\\*|__|~~)", "", format_exc())
+            await message.edit(f"<b>Произошла ошибка</b>:\n\n<code>{traceback}</code>")
+        if filename:
+            os.remove('/tmp/' + filename)
