1c863ef2dd82f3f449c8d5374ea83befce278e4f
Kompot <69313786+kompot69@users.noreply.github.com>
2024-01-16 20:14:25 +0300

    v2

--- FileDownloader.py
+++ FileDownloader.py
@@ -1,13 +1,12 @@
-# Friendly Telegram File Downloader (Uploader to Telegram) module by @kompot_69 & @mirivan
+# Friendly Telegram File Downloader (Uploader to Telegram) v2 module by @kompot_69 & @mirivan
 # requires: requests
-import datetime as DT
+import datetime
 import logging
 import os
 import re
 import requests
 import sys
 
-from datetime import datetime
 from io import BytesIO
 from telethon.errors import MessageIdInvalidError, MessageNotModifiedError
 from traceback import format_exc
@@ -15,6 +14,9 @@ from urllib.parse import urlparse
 
 
 from .. import loader, utils
+
+delta = datetime.timedelta(seconds=3)
+tasks = {}
 
 def sizeof_fmt(num):
     for unit in ["B", "KB", "MB", "GB"]:
@@ -26,7 +28,7 @@ def sizeof_fmt(num):
 
 @loader.tds
 class FileDownloaderMod(loader.Module):
-    strings = {"name": "File Downloader"}
+    strings = {"name": "File Downloader v2"}
 
     async def client_ready(self, client, db):
         self.client = client
@@ -43,7 +45,7 @@ class FileDownloaderMod(loader.Module):
         usage = "<b>Использование модуля</b>:\n" + "\n".join(commands)
         reply = await message.get_reply_message()
         arg = utils.get_args_raw(message)
-        url_regex = "((?:https?:\/\/)[A-z0-9]{2,}\.(?:[A-z]{2,})?[^\s]+[^. ])"
+        url_regex = r"(?:https?:\/\/)[A-Za-z0-9]{2,}.(?:[A-Za-z]{2,})?[^\s]+"
 
         match_arg = re.findall(url_regex, arg)
         match_reply = re.findall(url_regex, reply.raw_text) if reply and reply.raw_text else None
@@ -51,19 +53,21 @@ class FileDownloaderMod(loader.Module):
             url = match_arg[0] if match_arg else match_reply[0]
             args = arg.split(" ", 1)
             last_args_index = len(args) - 1
-            text = args[-1]
-            if url in args and len(args) > 1:
-                if not args.index(url) == last_args_index:
-                    text = args[-2]
+            text = None
+            if len(args) > 1:
+                if args.index(url) == last_args_index:
+                    return await message.edit("1 "+usage)
+                else:
+                    text = args[1]
 
             if match_reply and len(match_reply) > 1:
                 index = args[0]
                 if index.isnumeric() and int(index) > 0:
                     url = match_arg[int(index) - 1] if match_arg else match_reply[int(index) - 1]
                 else:
-                    return await message.edit(usage)
+                    return await message.edit("2 "+usage)
         else:
-            return await message.edit(usage)
+            return await message.edit("3 "+usage)
 
         try:
             with requests.get(url.strip(), headers={'Accept-Encoding': None}, stream=True, timeout=(10, 5), verify=False) as r:
@@ -77,37 +81,63 @@ class FileDownloaderMod(loader.Module):
                 if not length:
                     return await message.edit("<b>Не удалось получить размер файла.</b>")
                 me = await message.client.get_me()
-                allowed_file_size = 4 if me.premium else 2
+                is_premium =  getattr(me, "premium", False)
+                allowed_file_size = 4 if is_premium else 2
                 if length > allowed_file_size * 1024 ** 3:
                     return await message.edit(f"<b>Файл не будет загружен, поскольку его размер (</b><code>{sizeof_fmt(length)}</code><b>) превышает допустимые для вас <code>{allowed_file_size}GB</code> для загрузки файлов.")
                 length_h = sizeof_fmt(length)
-                timestamp_old = timestamp_new = datetime.now()
                 downloaded = 0
-                await message.edit(f"<b>Загружаю файл</b>: <code>{filename}</code>")
-                with open('/tmp/' + filename, 'wb') as f:
+                message = await message.edit(f"<b>Загружаю файл</b>: <code>{filename}</code>")
+                with open('/tmp/' + filename, 'wb') as file:
                     for chunk in r.iter_content(chunk_size=8192):
-                        f.write(chunk)
+                        file.write(chunk)
                         downloaded += len(chunk)
                         done = int(20 * downloaded / length)
                         percent = round(downloaded / length * 100, 1)
-                        if timestamp_old >= timestamp_new:
-                            try:
-                                await message.edit(f"<b>Загружаю файл</b>: <code>{filename}</code>\n{percent}% [<code>{'=' * done}{' ' * (20-done)}</code>] <code>{sizeof_fmt(downloaded)}/{length_h}</code>")
-                            except MessageNotModifiedError:
-                                pass
-                            timestamp_new += DT.timedelta(seconds=5)
-                        else:
-                            timestamp_old = datetime.now()
-                    await message.edit(f"<b>Выгружаю файл</b>: <code>{filename}</code>")
-                    caption = text or f"<b>Файл загружен по ссылке</b>: <code>{url}</code>"
-                    await message.client.send_file(message.to_id, '/tmp/' + filename, caption=caption, reply_to=reply)
-                    await message.delete()
+                        now = datetime.datetime.now(tz=datetime.timezone.utc)
+                        if message.edit_date and now > message.edit_date + delta:
+                            message = await message.edit(f"<b>Загружаю файл</b>: <code>{filename}</code>\n{percent}% [<code>{'=' * done}{' ' * (20-done)}</code>] <code>{sizeof_fmt(downloaded)}/{length_h}</code>")
+                    file.close()
+            await message.client.send_file(
+                message.to_id, '/tmp/' + filename,
+                caption=(
+                    text or f"<b>Файл загружен по ссылке</b>: <code>{url}</code>"
+                ),
+                reply_to=reply,
+                progress_callback=lambda d, t: message.client.loop.create_task(
+                    progress(d, t, message, filename)
+                )
+            )
+            await message.delete()
+            del tasks[message.id]
         except MessageIdInvalidError:
             await message.client.send_message(message.to_id, "<b>Загрузка файла остановлена пользователем.</b>", reply_to=reply)
+        except MessageNotModifiedError:
+            pass
         except requests.exceptions.Timeout:
             await message.edit(f"<b>Истекло время ожидания от сервера</b>: <code>{url}</code>")
         except Exception as e:
             traceback = re.sub("(`|\\|\\||\\*\\*|__|~~)", "", format_exc())
             await message.edit(f"<b>Произошла ошибка</b>:\n\n<code>{traceback}</code>")
         if filename:
-            os.remove('/tmp/' + filename)
+            try:
+                os.remove("/tmp/" + filename, True)
+            except:
+                await message.client.send_message(message.to_id, f"<b>Ошибка удаления временного файла</b>:\n\n<code>{format_exc()}</code>", reply_to=reply)
+
+async def progress(current, total, message, filename):
+    if not message.id in tasks:
+        message = await message.edit(f"<b>Выгружаю файл</b>: <code>{filename}</code>")
+        tasks[message.id] = message.edit_date + delta
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    if now > tasks[message.id]:
+        percent = round(current / total * 100, 1)
+        done = int(20 * current / total)
+        current = sizeof_fmt(current)
+        total = sizeof_fmt(total)
+        progress = f"{percent}% [<code>{'=' * done}{' ' * (20-done)}</code>] <code>{current}/{total}</code>"
+        try:
+            message = await message.edit(f"<b>Выгружаю файл</b>: <code>{filename}</code>\n{progress}")
+            tasks[message.id] = message.edit_date + delta
+        except MessageNotModifiedError:
+            pass
